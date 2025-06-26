22dd3620e59b4bb71c6a81a34dfc91fd33619212
Risina Liliia <risinalilia2@gmail.com>
2025-06-26 22:46:14 +0200

    validation fixed

--- src/validation/authSchemas.js
+++ src/validation/authSchemas.js
@@ -1,12 +1,12 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')),
+  name: Joi.string().max(16).required(),
+  email: Joi.string().email().max(128).required(),
+  password: Joi.string().min(8).max(128).required(),
 });
 
 export const loginUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')),
+  email: Joi.string().email().max(128).required(),
+  password: Joi.string().min(8).max(128).required(),
 });
