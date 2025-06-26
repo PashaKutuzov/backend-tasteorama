1c1b0d876b6494cee751a17a4697b35eb76d4e42
PashaKutuzov <pasha260605@gmail.com>
2025-06-26 18:57:09 +0300

    changes

--- src/routers/recipes.js
+++ src/routers/recipes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import ctrlWrapper from '../validation/ctrlWrapper.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 import {
   getRecipesController,
   getRecipesByIdController,
