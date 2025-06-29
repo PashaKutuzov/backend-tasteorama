98dcc0c46db55b9bc07a9d5b9240393a70d6d604
Risina Liliia <risinalilia2@gmail.com>
2025-06-29 11:51:53 +0200

    fix rout user/me

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,20 @@
 import { UsersCollection } from '../models/userModel.js';
 import '../models/recipesModel.js';
 
-export const getCurrentUser = async (req, res, next) => {
+export const getCurrentUser = async (req, res) => {
   const user = await UsersCollection.findById(req.user._id)
     .populate('favorites')
     .populate('myRecipes');
 
   res.status(200).json({
-    id: user._id,
-    name: user.name,
-    email: user.email,
-    favorites: user.favorites,
-    myRecipes: user.myRecipes,
+    status: 200,
+    message: 'User info retrieved successfully',
+    data: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      favorites: user.favorites,
+      myRecipes: user.myRecipes,
+    },
   });
 };
--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { getCurrentUser } from '../controllers/userController.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
-router.get('/me', authenticate, getCurrentUser);
-
+router.get('/me', authenticate, ctrlWrapper(getCurrentUser));
 export default router;
